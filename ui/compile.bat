4feb4bf988e5d6a34dbfa4372d48b32013f19a72
Stephan Robinig <stephan.robinig@student.tugraz.at>
2023-03-13 02:28:11 +0100

    Update bat install script

--- ui/compile.bat
+++ ui/compile.bat
@@ -1,2 +1,7 @@
 @ECHO off
-python3 -m nuitka --onefile --include-qt-plugins=sensible,styles,qml --plugin-enable=pyside6 --include-data-dir=qml=qml --follow-imports main.py
+.\venv\Scripts\python.exe -m nuitka --standalone --include-qt-plugins=all --plugin-enable=pyside6 --include-data-dir=qml=qml --follow-imports main.py
+copy .\venv\Lib\site-packages\PySide6\QtWebEngineProcess.exe .\main.dist\PySide6\QtWebEngineProcess.exe
+xcopy.exe .\venv\Lib\site-packages\PySide6\resources\ .\main.dist\PySide6\resources\ /s
+mkdir .\main.dist\resources\tmp
+xcopy.exe .\base\ .\main.dist\base\ /s
+copy .\resources\icon.svg .\main.dist\resources\icon.svg
