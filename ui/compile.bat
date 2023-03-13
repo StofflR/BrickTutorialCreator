f6fe4d393c0de893848c11f98d45e7a6a51f287b
Stephan Robinig <stephan.robinig@student.tugraz.at>
2023-03-13 15:12:38 +0100

    Update windows compile script

--- ui/compile.bat
+++ ui/compile.bat
@@ -1,7 +1,11 @@
 @ECHO off
-.\venv\Scripts\python.exe -m nuitka --standalone --include-qt-plugins=all --plugin-enable=pyside6 --include-data-dir=qml=qml --follow-imports main.py
-copy .\venv\Lib\site-packages\PySide6\QtWebEngineProcess.exe .\main.dist\PySide6\QtWebEngineProcess.exe
-xcopy.exe .\venv\Lib\site-packages\PySide6\resources\ .\main.dist\PySide6\resources\ /s
-mkdir .\main.dist\resources\tmp
-xcopy.exe .\base\ .\main.dist\base\ /s
-copy .\resources\icon.svg .\main.dist\resources\icon.svg
+.\venv\Scripts\python.exe -m nuitka --standalone --include-qt-plugins=all --plugin-enable=pyside6 --include-data-dir=qml=qml --windows-icon-from-ico=.\resources\icon.ico --follow-imports BrickTutorialCreator.py
+copy .\venv\Lib\site-packages\PySide6\QtWebEngineProcess.exe .\BrickTutorialCreator.dist\PySide6\QtWebEngineProcess.exe
+xcopy.exe .\venv\Lib\site-packages\PySide6\resources\ .\BrickTutorialCreator.dist\PySide6\resources\ /s
+mkdir .\BrickTutorialCreator.dist\resources\tmp
+xcopy.exe .\base\ .\BrickTutorialCreator.dist\base\ /s
+copy .\resources\icon.svg .\BrickTutorialCreator.dist\resources\icon.svg
+echo "Zipping Application ..."
+ren BrickTutorialCreator.dist BrickTutorialCreator
+tar.exe -a -cf BrickTutorialCreator.zip BrickTutorialCreator
+ren BrickTutorialCreator BrickTutorialCreator.dist
