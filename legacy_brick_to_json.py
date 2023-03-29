92da0560c105376403d61efaf2e64cb7606212f2
Stephan Robinig <stephan.robinig@student.tugraz.at>
2023-03-29 02:30:16 +0200

    WIP legacy brick converter

--- legacy_brick_to_json.py
+++ legacy_brick_to_json.py
@@ -1,5 +1,5 @@
 import re
-
+import ui.modules.SVGBrick as SVGBrick
 colorDict = {
     "408ac5": "brick_blue"
 }
@@ -54,17 +54,38 @@ def findText(text):
     print(text := text.replace("\n", "").replace("\t"," ").strip(" "))
     end = text[0:-1].find("<ns0:text id=\"text\" ")
     data = []
-    while(search):
+    start_line = start_poly = start_text = 0
+    while start_poly > -1 or start_line > -1 or start_text > -1:
         start_text = text[end:-1].find("<ns0:text id=\"text\" ")
         start_poly = text[end:-1].find("<ns0:polygon ")
-        start_line = text[end:-1].find("<<ns0:line ")
+        start_line = text[end:-1].find("<ns0:line ")
+        text = text[end:-1]
+        search_line = (start_line < start_poly or start_poly < 0) and (
+                    start_line < start_text or start_text < 0) and start_line >= 0
+        search_poly = (start_poly < start_line or start_line < 0) and (
+                    start_poly < start_text or start_text < 0) and start_poly >= 0
+        search_text = (start_text < start_poly or start_poly < 0) and (
+                    start_text < start_line or start_line < 0) and start_text >= 0
 
-        start = start + text[start:-1].find(";\">")
-        end = start + text[start:-1].find("</ns0:text>")
-        search = start < end
-        if(search):
-            print(text[start+3:end])
-            data.append(text[start+3:end])
+        if search_text:
+            start_text = text.find(";\">")
+            end = text.find("</ns0:text>")
+            if start_poly <= end:
+                print(text[start_text + 3:end])
+                data.append(text[start_text + 3:end])
+            end = end + len("</ns0:text>")
+
+        if search_poly:
+            end = text.find("/>")
+            end = end + 3
+            if start_poly <= end:
+                data.append("poly")
+
+        if search_line:
+            end = text.find("/>")
+            end = end + 3
+            if start_line <= end:
+                data.append("line")
     return data
 
 
@@ -77,8 +98,41 @@ def convert(brick):
     return {"color": colorDict[color], "height": heigthDict[heigth], "data": data}
 
 
+def parse(param):
+    line = False
+    poly = False
+    nl = False
+    string = ""
+    for element in param:
+        if element == "nl":
+            string += "\n"
+        elif element != "line" and element != "poly":
+            pad = "" + "$" * line + "*" * poly
+            string = string + pad + element + pad
+        line = element == "line"
+        poly = element == "poly"
+    return string
+
+
 if __name__ == '__main__':
-    bricks = ["bricks/ARDrone/Emergency_AR.Drone_2.0.svg"]
+    f = []
+    root_dir = "bricks"
+    file_set = []
+
+    for dir_, _, files in os.walk(root_dir):
+        for file_name in files:
+            rel_dir = os.path.relpath(dir_, root_dir)
+            rel_file = os.path.join(rel_dir, file_name)
+            file_set.append(os.path.join(root_dir, rel_file))
+    print(file_set)
+    bricks = [r"bricks\Sound\default\Speak_'Hello'_and_wait.svg"]
+    base = r"..\..\BrickTutorialCreator\ui\base"
     for brick in bricks:
+        print(brick)
         data = convert(brick)
         print(data)
+        content = parse(data["data"])
+        print(content)
+        brick = SVGBrick.SVGBrick(data["color"], content, data["height"].replace("h", ""), base+"\\brick_"+data["color"]+"_"+data["height"]+".svg")
+        brick.save()
+        print("asd")
