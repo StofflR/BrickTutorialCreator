22022512903ad0acf1bed4c04c296d4b42dc064b
Stephan Robinig <stephan.robinig@student.tugraz.at>
2023-11-27 22:14:13 +0100

    wip unit test

--- ui/tests/basic_bricks/test_basic_bricks_1h.py
+++ ui/tests/basic_bricks/test_basic_bricks_1h.py
@@ -1,24 +1,76 @@
 import pytest
-from modules.backend.SVGBrick import SVGBrick
+import sys
 from os import getcwd, path
+
+import scipy
+import numpy
+import cv2
+
+sys.path.insert(0, getcwd())
+from modules.backend.SVGBrick import SVGBrick
+from tests.initializers import initQt
+
+initQt()
+
+data = {}
+data["sizes"] = ["1h", "2h", "3h"]
+data["colors"] = [
+    "cyan",
+    "dark_blue",
+    "gold",
+    "dark_green",
+    "green",
+    "light_orange",
+    "olive",
+    "orange",
+    "yellow",
+    "violet",
+    "pink",
+    "red",
+    "white",
+    "transparent_white",
+    "transparent_black",
+]
 
 
 def difference(img_a, img_b):
-    pass
+    img1 = cv2.imread(img_a, cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(img_b, cv2.IMREAD_GRAYSCALE)
+    norm1, norm2 = compare_images(img1, img2)
+    print(norm1, norm2)
+    return 0
 
+def compare_images(img1, img2):
+    # normalize to compensate for exposure difference, this may be unnecessary
+    # consider disabling it
+    img1 = numpy.normalize(img1)
+    img2 = numpy.normalize(img2)
+    # calculate the difference and its norms
+    diff = img1 - img2  # elementwise for scipy arrays
+    m_norm = scipy.sum(scipy.abs(diff))  # Manhattan norm
+    z_norm = scipy.linalg.norm(diff.ravel(), 0)  # Zero norm
+    return (m_norm, z_norm)
 
 def test_bricks():
-    brick_type = "dark_green"
-    created_path = path.join(getcwd(), brick_type + ".png")
-    ref_path = path.join(getcwd(), "ref/" + brick_type + ".png")
-    brick = SVGBrick(
-        base_type=brick_type,
-        content="",
-        size="1h",
-        path="brick_dark_green_1h.svg",
-        scaling_factor=1.0,
-        x=43,
-        y=33,
-    )
-    brick.savePNG(path=created_path)
-    assert difference(created_path, ref_path) < 0.1
+    for size in data["sizes"]:
+        for brick_type in data["colors"]:
+            brick_path = f"brick_{brick_type}_{size}.svg"
+            ref_path = path.join(
+                getcwd(), r"tests\\ref\\" + f"{brick_type}_{size}" + ".png"
+            )
+            brick = SVGBrick(
+                base_type=brick_type,
+                content="",
+                size=size,
+                path=brick_path,
+                scaling_factor=1.0,
+                x=43,
+                y=33,
+            )
+            created_path = brick.working_brick_.replace(".svg", ".png")
+            brick.savePNG(path=created_path)
+            assert difference(ref_path, created_path) < 0.1
+
+
+if __name__ == "__main__":
+    test_bricks()
---
+++ ui/tests/initializers.py
@@ -0,0 +1,6 @@
+from PySide6.QtGui import QGuiApplication
+import sys
+
+
+def initQt():
+    app = QGuiApplication(sys.argv)
