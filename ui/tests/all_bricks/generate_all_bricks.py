26c7c0d3c5b218fe74c83d2b1b5ae01d43710c1f
StofflR <StofflR@users.noreply.github.com>
2024-01-03 22:41:45 +0000

    :art: Format Python code with psf/black

--- ui/tests/all_bricks/generate_all_bricks.py
+++ ui/tests/all_bricks/generate_all_bricks.py
@@ -9,18 +9,20 @@ from modules.backend.SVGBrick import SVGBrick
 from tests.initializers import initQt
 import json
 
-def iterate_files(folder_path) -> None: 
+
+def iterate_files(folder_path) -> None:
     for root, dirs, files in walk(folder_path):
         for file_name in files:
             file_path = path.join(root, file_name)
             if ".json" in file_path:
                 print("opening", file_path)
-                file = open(file_path, 'r')
+                file = open(file_path, "r")
                 content = json.load(file)
                 brick: SVGBrick = SVGBrick.fromJSON(content)
                 file.close()
                 brick.savePNG(path=file_path.replace(".json", ".png"))
                 brick.save(path=file_path.replace(".json", ".svg"))
+
 
 if __name__ == "__main__":
     ref_brick_path = "tests/ref_all_bricks"
--- ui/tests/all_bricks/test_all_bricks.py
+++ ui/tests/all_bricks/test_all_bricks.py
@@ -9,6 +9,7 @@ sys.path.insert(0, getcwd())
 from modules.backend.SVGBrick import SVGBrick
 from tests.initializers import initQt
 
+
 def compare_images(img1, img2):
     # Convert images to grayscale if needed
     image1 = skimage.io.imread(img1)
@@ -20,6 +21,7 @@ def compare_images(img1, img2):
     print("comparing: ", img1, img2, ssim_value)
     return ssim_value
 
+
 def test_all_bricks():
     ref_brick_path = "tests/ref_all_bricks"
     for root, dirs, folder in walk(ref_brick_path):
@@ -27,13 +29,14 @@ def test_all_bricks():
             file_path = os.path.join(root, file_name)
             if ".json" in file_path:
                 print("opening", file_path)
-                file = open(file_path, 'r')
+                file = open(file_path, "r")
                 content = json.load(file)
                 brick: SVGBrick = SVGBrick.fromJSON(content)
                 file.close()
-                path=brick.working_brick_.replace(".json", ".png")
-                brick.savePNG(path=path) 
+                path = brick.working_brick_.replace(".json", ".png")
+                brick.savePNG(path=path)
                 assert compare_images(path, file_path) > 0.995
+
 
 if __name__ == "__main__":
     test_all_bricks()
