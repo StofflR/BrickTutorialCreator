6094ef903d76d06878b4bf067db6ddd4c56230db
Stephan Robinig <stephan.robinig@student.tugraz.at>
2024-08-28 20:54:12 +0200

    adapt drawable generation script

--- ui/resources/basic_brick_generator.py
+++ ui/resources/basic_brick_generator.py
@@ -1,5 +1,7 @@
 import os
 from os import getcwd, path
+import sys
+sys.path.insert(0, getcwd())
 from modules.backend.SVGBrick import SVGBrick
 from tests.initializers import initQt, compareImages
 import modules.ConstDefs as Const
@@ -7,6 +9,29 @@ from modules.ConstDefs import *
 from PIL import Image
 
 output_folder = os.path.join(os.getcwd(), "drawable")
+
+drawable_dpis = {}
+drawable_dpis["mdpi"] = { "width" : 56, 
+                          "1h" : 46,
+                          "2h" : 73, 
+                          "3h" : 96}
+drawable_dpis["hdpi"] = { "width" : 83, 
+                          "1h" : 66,
+                          "2h" : 109, 
+                          "3h" : 143}
+drawable_dpis["xhdpi"] = { "width" : 111, 
+                          "1h" : 90,
+                          "2h" : 144, 
+                          "3h" : 190}   
+drawable_dpis["ldpi"] = { "width" : 43, 
+                          "1h" : 35,
+                          "2h" : 55, 
+                          "3h" : 73}
+drawable_dpis["xxhdpi"] = { "width" : 164, 
+                          "1h" : 134,
+                          "2h" : 216, 
+                          "3h" : 284}
+
 if not os.path.isdir(output_folder):
     os.mkdir(output_folder)
 
@@ -36,26 +61,27 @@ def generate_bricks(data):
             )
 
             # height = Const.PNG_HEIGHT_1H
-            height = 66
-            width = 81
+            dpi = drawable_dpis["hdpi"]
+            height = dpi["1h"]
+            width = dpi["width"]
             export_width = Const.PNG_WIDTH / Const.PNG_HEIGHT_1H * height
             if "2h" in size:
-                height = 108
+                height = dpi["2h"]
             #    height = Const.PNG_HEIGHT_2H
             elif "3h" in size:
-                height = 142
+                height = dpi["3h"]
             #    height = Const.PNG_HEIGHT_3H
             export_path = os.path.join(
                 output_folder, brick_path.replace(SVG_EXT, PNG_EXT)
             )
             brick.savePNG(export_path, width=export_width, height=height)
-            crop_width(export_path, width, export_path.replace("png", "9.png"))
+            crop_width(export_path, width, export_path.replace(".png", "9.png"))
 
 
 def generate_control_bricks(data):
     for size in data["sizes"]:
         for brick_type in data["colors"]:
-            brick_path = f"brick_{brick_type}_{size}_control.svg"
+            brick_path = f"brick_{brick_type}_{size}_ctrol.svg"
             brick = SVGBrick(
                 base_type=brick_type,
                 content="",
@@ -96,9 +122,11 @@ data["colors"] = [
     "pink",
     "red",
     "white",
+    "winered"
     # "transparent_white",
     # "transparent_black",
 ]
+data["colors"] = ["winered"]
 initQt()
 generate_bricks(data)
 data["sizes"] = ["1h", "2h"]
